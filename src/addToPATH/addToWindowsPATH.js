18f0521e841d66fda7e13522319f1b190692837f
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2026-04-06 11:31:33 -0400

    addToWindowsPATH (#9)

--- src/addToPATH/addToWindowsPATH.js
+++ src/addToPATH/addToWindowsPATH.js
@@ -2,15 +2,7 @@
  * @file Logic related to adding a folder to the Windows PATH.
  */
 
-/**
- * Reference existing library implementations:
- *
- * https://git.rootprojects.org/root/pathman/src/branch/master/npm
- * Just downloads a binary to add/remove from the PATH for you. The source code is actually GO.
- *
- * https://github.com/MarkTiedemann/win-path
- * Only does Windows, requires powershell already in PATH (should be fine), uses CJS, published 2017
- */
+import { execSync } from 'node:child_process';
 
 /** @typedef {import('../types').LOGGER} LOGGER */
 
@@ -21,5 +13,68 @@
  * @param {LOGGER} logger  Function to handle logging
  */
 export const addToWindowsPATH = function (folder, logger) {
-  logger('STUB: Windows', folder);
+  const registryPATHLocation = [
+    'HKLM',
+    'SYSTEM',
+    'CurrentControlSet',
+    'Control',
+    'Session Manager',
+    'Environment'
+  ].join('\\');
+  const registryCommand = 'reg query "' + registryPATHLocation + '" /v Path';
+
+  let registryPATHResponse = '';
+  try {
+    registryPATHResponse = String(execSync(registryCommand));
+  } catch (error) {
+    logger('Error reading PATH data from Windows Registry.', error);
+    return;
+  }
+
+  // '\r\nHKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment\r\n    Path    ' +
+  // 'REG_EXPAND_SZ    C:\\WINDOWS;%USERPROFILE%\\.example;\r\n\r\n'
+  /* v8 ignore next */
+  const fullSystemPATH = registryPATHResponse
+    // ['\r\nHKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment\r\n    Path    ',
+    // '    C:\\WINDOWS;%USERPROFILE%\\.example;\r\n\r\n']
+    ?.split('REG_EXPAND_SZ')
+    // '    C:\\WINDOWS;%USERPROFILE%\\.example;\r\n\r\n'
+    ?.[1] || '';
+
+  const foldersInSystemPATH = fullSystemPATH
+    // 'C:\\WINDOWS;%USERPROFILE%\\.example;'
+    ?.trim()
+    // ['C:\\WINDOWS', '%USERPROFILE%\\.example', '']
+    ?.split(';')
+    // ['C:\\WINDOWS', '%USERPROFILE%\\.example']
+    ?.filter(Boolean);
+
+  // Return early if the folder to add to the PATH is already on the PATH
+  if (foldersInSystemPATH?.includes(folder)) {
+    return;
+  }
+
+  const newSystemPATH = [
+    folder,
+    ...fullSystemPATH
+  ].join(';');
+
+  let errorOccurred = false;
+  try {
+    // Replace the system PATH
+    execSync('setx PATH "' + newSystemPATH + '" /M');
+  } catch (error) {
+    errorOccurred = true;
+    if (error?.message?.includes('Access to the registry path is denied.')) {
+      logger(
+        'Adding a folder to the PATH requires administrator privileges to edit the registry.\n' +
+        'Try again using "Run as administrator".'
+      );
+    } else {
+      logger('Error adding folder to PATH', error);
+    }
+  }
+  if (!errorOccurred) {
+    logger('Folder added to PATH, restart terminals/shells/command prompts to access folder in PATH.');
+  }
 };
--- tests/unit/src/addToPATH/addToWindowsPATH.test.js
+++ tests/unit/src/addToPATH/addToWindowsPATH.test.js
@@ -1,13 +1,82 @@
+import { execSync } from 'node:child_process';
+
 import { addToWindowsPATH } from '@/addToPATH/addToWindowsPATH.js';
+
+vi.mock('node:child_process', () => {
+  return {
+    execSync: vi.fn()
+  };
+});
+
+const error = 'error';
+const mockedExecSync = vi.mocked(execSync);
+const registryResponse = (folder) => {
+  folder = folder || '';
+  return (
+    '\r\n' +
+    'HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment' +
+    '\r\n' +
+    '    Path    REG_EXPAND_SZ    C:\\WINDOWS;%USERPROFILE%\\.example;' + folder +
+    '\r\n\r\n'
+  );
+};
 
 describe('addToWindowsPATH', () => {
   const folder = 'C:\\folder';
   const logger = vi.fn();
 
-  test('STUB: Does nothing', () => {
+  beforeEach(() => {
+    vi.resetAllMocks();
+  });
+
+  test('Reading PATH from registry fails', () => {
+    mockedExecSync.mockThrow(error);
     addToWindowsPATH(folder, logger);
 
     expect(logger)
-      .toHaveBeenCalledWith('STUB: Windows', folder);
+      .toHaveBeenCalledWith('Error reading PATH data from Windows Registry.', error);
+  });
+
+  test('Returns early if folder exists on PATH', () => {
+    mockedExecSync
+      .mockReturnValueOnce(registryResponse(folder))
+      .mockThrow(error);
+    addToWindowsPATH(folder, logger);
+
+    expect(logger)
+      .not.toHaveBeenCalled();
+  });
+
+  test('Logs specific message about running as admin', () => {
+    mockedExecSync
+      .mockReturnValueOnce(registryResponse())
+      .mockThrow(new Error('ERROR: Access to the registry path is denied.'));
+    addToWindowsPATH(folder, logger);
+
+    expect(logger)
+      .toHaveBeenCalledWith(
+        'Adding a folder to the PATH requires administrator privileges to edit the registry.\n' +
+        'Try again using "Run as administrator".'
+      );
+  });
+
+  test('Logs non-specific error when setx fails', () => {
+    mockedExecSync
+      .mockReturnValueOnce(registryResponse())
+      .mockThrow(error);
+    addToWindowsPATH(folder, logger);
+
+    expect(logger)
+      .toHaveBeenCalledWith('Error adding folder to PATH', error);
+  });
+
+  test('Runs without failures', () => {
+    mockedExecSync
+      .mockReturnValueOnce(registryResponse())
+      .mockReturnValueOnce(undefined);
+    addToWindowsPATH(folder, logger);
+
+    expect(logger)
+      .toHaveBeenCalledWith('Folder added to PATH, restart terminals/shells/command prompts to access folder in PATH.');
   });
 });
